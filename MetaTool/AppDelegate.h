e589304ede711ed3dbf83474d8b9d09b450a4d8b
empoc <empoc@me.com>
2015-12-03 22:15:53 -0700

    Initial Commit

---
+++ MetaTool/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MetaTool
+//
+//  Created by Chris Fletcher on 12/3/15.
+//  Copyright © 2015 Chris Fletcher. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MetaTool/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MetaTool
+//
+//  Created by Chris Fletcher on 12/3/15.
+//  Copyright © 2015 Chris Fletcher. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
