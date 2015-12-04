445caf687334f1f68b194b710f2357932ed4dccd
empoc <empoc@me.com>
2015-12-03 22:17:28 -0700

    Added API Class

---
+++ MetaTool/API.h
@@ -0,0 +1,13 @@
+//
+//  API.h
+//  MetaTool
+//
+//  Created by Chris Fletcher on 12/3/15.
+//  Copyright © 2015 Chris Fletcher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface API : NSObject
+
+@end
---
+++ MetaTool/API.m
@@ -0,0 +1,13 @@
+//
+//  API.m
+//  MetaTool
+//
+//  Created by Chris Fletcher on 12/3/15.
+//  Copyright © 2015 Chris Fletcher. All rights reserved.
+//
+
+#import "API.h"
+
+@implementation API
+
+@end
